258fb8f7e845baa60c27ebf8f5983c5309b5f589
HANS TELMAN <guns1982@live.com>
2024-10-18 16:54:54 +0300

    Add Goodbye

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ def print_hi(name):
 
 def print_hello(name):
     print(f'Hello", {name}')
+
+def print_goodbye(name):
+    print(f'Goodbye", {name}')
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hello('PyCharm')
+    print_goodbye('Pop')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
